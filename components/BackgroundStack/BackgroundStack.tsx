064fdbae88b8137bb283b4a41b844e3fe7d7b01d
Ruslan Serhiienko <34864175+SerhiienkoRuslan@users.noreply.github.com>
2024-02-23 15:54:43 +0200

    ref: layout fix (#1)
    * ref: layout fix
    
    * ref: error fix
    
    * ref: updated background
    
    * ref: updated background
    
    * ref: updated background

--- components/BackgroundStack/BackgroundStack.tsx
+++ components/BackgroundStack/BackgroundStack.tsx
@@ -18,8 +18,7 @@ const BackgroundStack = ({ children, spacing }: PropsType) => {
       sx={{
         padding: "20px 0 0 0",
         height: "100vh",
-        backgroundImage:
-          "url('https://i.pinimg.com/originals/54/f8/13/54f81331a9da88c623b96363fb0a4da1.gif')",
+        backgroundImage: 'url(/bg.gif)',
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
       }}
